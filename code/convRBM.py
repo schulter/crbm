af0d767dcdfda7382188596c22feee923aa784c2
Roman <r.schulte-sasse@fu-berlin.de>
2016-02-03 10:36:20 +0100

    Pos phase used probs while neg phase used sample.
    There was a bug in the training procedure (updateWeightsOnMinibatch)
    in that the hidden probabilities were used to compute the statistics
    for the data (positive phase) and a sample was used to compute it
    for the model (negative phase).
    This could result in the wrong statistics collected.

--- code/convRBM.py
+++ code/convRBM.py
@@ -168,8 +168,8 @@ class CRBM:
 
     def computeVgivenH (self, H_sample):
         # dimshuffle the motifs to have K as channels (will be summed automatically)
-        W = self.motifs.dimshuffle(1, 0, 2, 3)#[:,:,::-1,:] # needs that due to miraculous reasons
-        C = conv.conv2d(H_sample, W, border_mode='full')#[:,:,::-1,:]
+        W = self.motifs.dimshuffle(1, 0, 2, 3)
+        C = conv.conv2d(H_sample, W, border_mode='full')
         if self.debug:
             C = theano.printing.Print('Pre sigmoid visible layer: ')(C)
         out = T.sum(C, axis=1, keepdims=True) # sum over all K
@@ -247,7 +247,7 @@ class CRBM:
             prob_of_H_given_model, H_given_model = self.computeHgivenV(V_given_model)
         
         # compute the model gradients
-        G_motif_model, G_bias_model, G_c_model = self.collectUpdateStatistics(H_given_model, V_given_model)
+        G_motif_model, G_bias_model, G_c_model = self.collectUpdateStatistics(prob_of_H_given_model, V_given_model)
         
         if self.debug:
             G_motif_model = theano.printing.Print('Gradient for motifs (model): ')(G_motif_model)
