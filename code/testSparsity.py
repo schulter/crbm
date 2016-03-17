9de4a32c9e0f0b49a55fc52a25494f9c3d7327ec
Roman Schulte-Sasse <sasse@molgen.mpg.de>
2016-03-17 18:05:50 +0100

    Complete script to test sparsity constraints with SVM and MM.
    This script generates a beautiful ROC curve afterwards and also saves
    the scores for each method if you ever want to do something even more
    beautiful...

--- code/testSparsity.py
+++ code/testSparsity.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 from convRBM import CRBM
 import getData as dataRead
 import trainingObserver as observer
-
+import markovModel
 import numpy as np
 import random
 import time
 from datetime import datetime
 import os
+import cPickle
 
 from sklearn.metrics import roc_curve, auc
 from scipy import interp
+from sklearn import svm
 
 import theano
 import theano.tensor as T
@@ -21,6 +23,20 @@ import theano.tensor as T
 ########################################################
 # SET THE HYPER PARAMETERS
 allHyperParams = [
+    {
+        'number_of_motifs': 100,
+        'motif_length': 9,
+        'learning_rate': 0.5,
+        'doublestranded': False,
+        'pooling_factor': 4,
+        'epochs': 200,
+        'cd_k': 5,
+        'sparsity': 0.01,
+        'batch_size': 20,
+        'verbose': False,
+        'cd_method': 'pcd',
+        'momentum': 0.9
+    },
     {
         'number_of_motifs': 100,
         'motif_length': 9,
@@ -90,24 +106,10 @@ allHyperParams = [
         'verbose': False,
         'cd_method': 'pcd',
         'momentum': 0.9
-    },
-    {
-        'number_of_motifs': 100,
-        'motif_length': 9,
-        'learning_rate': 0.5,
-        'doublestranded': False,
-        'pooling_factor': 4,
-        'epochs': 200,
-        'cd_k': 5,
-        'sparsity': 50,
-        'batch_size': 20,
-        'verbose': False,
-        'cd_method': 'pcd',
-        'momentum': 0.9
     }
 ]
 
-testset_size = 100
+train_test_ratio = 0.1
 batchSize = 100
 ########################################################
 
@@ -122,8 +124,8 @@ def getObserver(model, title):
 def buildModelWithObservers(hyperParams, veri_data):
     model = CRBM(hyperParams=hyperParams)
     reconstruction_observer = observer.ReconstructionRateObserver(model,
-                                                                        veri_data,
-                                                                        "Reconstruction Rate Training Observer")
+                                                                  veri_data,
+                                                                  "Reconstruction Rate Training Observer")
     model.addObserver(reconstruction_observer)
     return model
 
@@ -161,16 +163,19 @@ def getFreeEnergyPoints(model, data):
     return M
 
 
-def plotROC(scores, numPositives, sparsities):
-    assert len(scores) == len(sparsities)
+def plotROC(scores, texts, labels):
+    assert len(scores) == len(texts) #== len(labels)
     fig = plt.figure(figsize=(14, 8))
-    y_true = np.concatenate( (np.ones(numPositives), np.zeros(scores[0].shape[0] - numPositives)), axis=0 )
 
     for i in range(len(scores)):
-        fpr, tpr, thresholds = roc_curve(y_true=y_true, y_score=scores[i])
+        fpr, tpr, thresholds = roc_curve(y_true=labels, y_score=scores[i])
         roc_auc = auc(fpr, tpr)
-        plt.plot(fpr, tpr, label='Sparsity lambda ' + str(sparsities[i]) + ' (AUC = %0.2f)' % roc_auc)
+        plt.plot(fpr, tpr, label=texts[i] + '(AUC = %0.2f)' % roc_auc)
+    
+    # plot the random line
+    plt.plot([0, 1], [0, 1], 'k--', label='Random')
 
+    # set general parameters for plotting and so on
     plt.ylim([0.0, 1.05])
     plt.xlim([-0.05, 1])
     plt.xlabel('False Positive Rate')
@@ -185,46 +190,93 @@ print "Reading the data..."
 start = time.time()
 seqReader = dataRead.SeqReader()
 data_stem = seqReader.readSequencesFromFile('../data/stemcells.fa')
-data_fibro = seqReader.readSequencesFromFile('../data/fibroblast.fa')
+data_fibro = seqReader.readSequencesFromFile('../data/fibroblast.fa')[:len(data_stem)]
 
-trainingData_stem = np.array(data_stem[testset_size:])
-testingData_stem = np.array(data_stem[:testset_size])
-trainingData_fibro = np.array(data_fibro[testset_size:])
-testingData_fibro = np.array(data_fibro[:testset_size])
+per_sc = np.random.permutation(len(data_stem))
+itest_sc = per_sc[:int(len(data_stem)*train_test_ratio)]
+itrain_sc = per_sc[int(len(data_stem)*train_test_ratio):]
+
+per_fibro = np.random.permutation(len(data_fibro))
+itest_fibro = per_fibro[:int(len(data_fibro)*train_test_ratio)]
+itrain_fibro = per_fibro[int(len(data_fibro)*train_test_ratio):]
+
+trainingData_stem = np.array([data_stem[i] for i in itrain_sc])
+testingData_stem = np.array([data_stem[i] for i in itest_sc])
+trainingData_fibro = np.array([data_fibro[i] for i in itrain_fibro])
+testingData_fibro = np.array([data_fibro[i] for i in itest_fibro])
 
 allTestingData = np.concatenate( (testingData_stem, testingData_fibro), axis=0 )
-numPositives = testingData_stem.shape[0]
+allTrainingData = np.concatenate( (trainingData_stem, trainingData_fibro), axis=0 )
+
 
 print "Data successfully read in " + str((time.time()-start)) + " seconds."
-print "Number of stemcell test samples: " + str(numPositives)
+print "Number of stemcell test samples: " + str(testingData_stem.shape[0])
 print "Number of fibroblast test samples: " + str(testingData_fibro.shape[0])
 
 scores = list()
-sparsities = list()
+texts = list()
 count = 1
 
-for hyper_params in allHyperParams:
-    # build model
-    learner_stem = buildModelWithObservers(hyper_params, testingData_stem)
-    learner_stem.printHyperParams()
-    learner_fibro = buildModelWithObservers(hyper_params, testingData_fibro)
+# add the markov model to scores
+print "Training Markov Model on data..."
+mm_sc = markovModel.MarkovModel()
+mm_sc.trainModel(trainingData_stem)
 
-    # train model
-    print "Train both models..."
-    start = time.time()
-    learner_stem.trainModel(trainingData_stem)
-    learner_fibro.trainModel(trainingData_fibro)
-    
-    # evaluate free energy for testing data
-    print "Get free energy for both models..."
-    score_sc = getFreeEnergyPoints(learner_stem, allTestingData)
-    score_fibro = getFreeEnergyPoints(learner_fibro, allTestingData)
-    subtracted = score_fibro - score_sc
-    
-    scores.append(subtracted)
-    sparsities.append(hyper_params['sparsity'])
+mm_fib = markovModel.MarkovModel()
+mm_fib.trainModel(trainingData_fibro)
 
-    print "SPARSITY CONSTRAINT " + str(hyper_params['sparsity']) + " DONE (" + str(count) + " / " + str(len(allHyperParams)) + ")"
-    count += 1
+scores_sc = mm_sc.evaluateSequences(allTestingData)
+scores_fib = mm_fib.evaluateSequences(allTestingData)
 
-plotROC(scores, numPositives, sparsities)
+scores.append(scores_sc - scores_fib)
+texts.append('First Order Markov Model')
+
+print "Training complete!"
+
+# add the SVM to the plot
+print "Training SVM on data..."
+km_train = dataRead.computeKmerCounts(allTrainingData, 5)
+km_test = dataRead.computeKmerCounts(allTestingData, 5)
+
+labels_for_svm = np.concatenate( (np.ones(trainingData_stem.shape[0]), -np.ones(trainingData_fibro.shape[0])), axis=0 )
+
+clf = svm.SVC(probability=True)
+clf.fit(km_train, labels_for_svm)
+
+scores.append(clf.predict_proba(km_test)[:,1])
+texts.append('SVM with RBF kernel')
+print "Training SVM complete"
+
+try:
+    for hyper_params in allHyperParams:
+        # build model
+        learner_stem = buildModelWithObservers(hyper_params, testingData_stem)
+        learner_stem.printHyperParams()
+        learner_fibro = buildModelWithObservers(hyper_params, testingData_fibro)
+
+        # train model
+        print "Train cRBM for both datasets..."
+        start = time.time()
+        learner_stem.trainModel(trainingData_stem)
+        learner_fibro.trainModel(trainingData_fibro)
+        
+        # evaluate free energy for testing data
+        print "Get free energy for both models..."
+        score_sc = getFreeEnergyPoints(learner_stem, allTestingData)
+        score_fibro = getFreeEnergyPoints(learner_fibro, allTestingData)
+        subtracted = score_fibro - score_sc
+
+        scores.append(subtracted)
+        texts.append('cRBM with sparsity ' + str(hyper_params['sparsity']))
+
+        print "SPARSITY CONSTRAINT " + str(hyper_params['sparsity']) + " DONE (" + str(count) + " / " + str(len(allHyperParams)) + ")"
+        count += 1
+except KeyboardInterrupt:
+    print "Ending the program now. But let me first save the scores calculated!"
+
+finally:
+    labels = np.concatenate( (np.ones(testingData_stem.shape[0]), np.zeros(testingData_fibro.shape[0])), axis=0 )
+    with open('scores_crbm.pkl', 'w') as f:
+        cPickle.dump( (scores, texts, labels), f)
+    plotROC(scores, texts, labels)
+
