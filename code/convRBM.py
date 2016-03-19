20205e098b99188b258e7a9505a6a1bd292ece30
wkopp <kopp@molgen.mpg.de>
2016-03-19 13:10:42 +0100

    use default random seed

--- code/convRBM.py
+++ code/convRBM.py
@@ -95,7 +95,7 @@ class CRBM:
         self.c = theano.shared(value=c, name='c', borrow=True)
 
         # infrastructural parameters
-        self.theano_rng = RS(seed=1234)
+        self.theano_rng = RS()
         self.params = [self.motifs, self.bias, self.c]
         
         self.debug = self.hyper_params['verbose']
