104f6cd1e88cf211cbeb1ef3c413736c776a242d
wkopp <kopp@molgen.mpg.de>
2017-07-19 10:29:32 +0200

    adapted interface

--- script/analyse_oct4_mafk_models.py
+++ script/analyse_oct4_mafk_models.py
@@ -4,91 +4,75 @@ import numpy as np
 import sys
 sys.path.append("../code")
 from convRBM import CRBM
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from getData import seqToOneHot, readSeqsFromFasta
+from Bio.motifs.matrix import PositionWeightMatrix
+from Bio import motifs
+from Bio.Alphabet import IUPAC
+
 import plotting
 
 outputdir = os.environ["CRBM_OUTPUT_DIR"]
 
-########################################################
-# SET THE HYPER PARAMETERS
-hyperParams={
-        'number_of_motifs': 10,
-        'motif_length': 15,
-        'input_dims':4,
-        'momentum':0.95,
-        'learning_rate': .5,
-        'doublestranded': True,
-        'pooling_factor': 1,
-        'epochs': 300,
-        'cd_k': 5,
-        'sparsity': 0.5,
-        'batch_size': 100,
-    }
-
 # generate cRBM models
-crbm_stem = CRBM(hyperParams=hyperParams)
-crbm_fibro = CRBM(hyperParams=hyperParams)
-crbm_merged = CRBM(hyperParams=hyperParams)
+crbm_oct4 = CRBM.loadModel(outputdir + "/oct4_model.pkl")
+crbm_mafk = CRBM.loadModel(outputdir + "/mafk_model.pkl")
+crbm_merged = CRBM.loadModel(outputdir + "/oct4_mafk_joint_model.pkl")
 
-crbm_stem.loadModel(outputdir + "/stem_model.pkl")
-crbm_fibro.loadModel(outputdir + "/fibro_model.pkl")
-crbm_merged.loadModel(outputdir + "/merged_model.pkl")
+tr_o = seqToOneHot(readSeqsFromFasta('../data/stemcells_train.fa'))
+te_o = seqToOneHot(readSeqsFromFasta('../data/stemcells_test.fa'))
+tr_m = seqToOneHot(readSeqsFromFasta('../data/fibroblast_train.fa'))
+te_m = seqToOneHot(readSeqsFromFasta('../data/fibroblast_test.fa'))
 
-training_stem, test_stem,training_fibro, test_fibro,training_merged,test_merged \
-        = joblib.load(\
-        outputdir + "dataset.pkl")
+te_merged = np.concatenate( (te_o, te_m), axis=0 )
+tr_merged = np.concatenate( (tr_o, tr_m), axis=0 )
+labels = np.concatenate( (np.ones(te_o.shape[0]),
+    np.zeros(te_m.shape[0])), axis=0 )
 
-# evaluate free energy for testing data
-print "Get free energy for both models..."
-score_sc = crbm_stem.freeEnergy(test_merged)
-score_fibro = crbm_fibro.freeEnergy(test_merged)
-score = score_fibro - score_sc
 
-labels = np.concatenate( (np.ones(test_stem.shape[0]), \
-        np.zeros(test_fibro.shape[0])), axis=0 )
+# motif 8 corresponds to Oct4
+#m = crbm_oct4.getPFMs()[8]
+#m = crbm_oct4.getPFMs()[8]
+def createLogo(pfm, name):
+    plt.imshow(plotting.createWeblogo(pfm, highRes=True))
+    ax = plt.axes()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    fig = ax.get_figure()
+    fig.savefig(outputdir + "crbm_" + name + ".eps", dpi=1000)
 
-plotting.plotROC(labels, score, outputdir + "roc_oct4_mafk.eps")
-plotting.plotPRC(labels, score, outputdir + "prc_oct4_mafk.eps")
-crbm_stem.printHyperParams()
+createLogo(crbm_oct4.getPFMs()[2], "Oct4-like1")
+createLogo(crbm_oct4.getPFMs()[5], "Oct4-like2")
+createLogo(crbm_mafk.getPFMs()[9], "Mafk-like1")
+createLogo(crbm_mafk.getPFMs()[3], "Mafk-like2")
 
-plt_stem = plotting.plotMotifsWithOccurrences(crbm_stem, test_stem, \
+# 
+plt_oct4 = plotting.plotMotifsWithOccurrences(crbm_oct4, te_o, \
         outputdir + "oct4_model_features.eps")
-plt_fibro = plotting.plotMotifsWithOccurrences(crbm_fibro, test_fibro, \
+plt_mafk = plotting.plotMotifsWithOccurrences(crbm_mafk, te_m, \
         outputdir + "mafk_model_features.eps")
-plt_all = plotting.plotMotifsWithOccurrences(crbm_merged, test_merged, \
+plt_all = plotting.plotMotifsWithOccurrences(crbm_merged, te_merged, \
         outputdir + "joint_model_features.eps")
 
-#per position TSNE
-X = plotting.runTSNEPerPosition(crbm_merged, test_merged)
-
-Xoct4, Xmafk = X[:test_stem.shape[0]], X[test_stem.shape[0]:]
-color =labels
-
-lims = (X.min(axis=0)-1, X.max(axis=0)+1)
-
-plotting.tsneScatter({"Oct4":Xoct4, "Mafk":Xmafk}, lims,
-            outputdir + "tsne_clustering_oct4_mafk_pp.pdf")
-
-plotting.plotTSNE_withpie(crbm_merged, test_merged, X, lims, \
-        outputdir + "tsne_clustering_pie_pp.pdf")
-
 #per sequence TSNE
-X = plotting.runTSNEPerSequence(crbm_merged, test_merged)
+X = plotting.runTSNEPerSequence(crbm_merged, te_merged)
 lims = (X.min(axis=0)-1, X.max(axis=0)+1)
 
-Xoct4, Xmafk = X[:test_stem.shape[0]], X[test_stem.shape[0]:]
+Xoct4, Xmafk = X[:te_o.shape[0]], X[te_o.shape[0]:]
 
 plotting.tsneScatter({"Oct4":Xoct4, "Mafk":Xmafk}, lims,
             outputdir + "tsne_clustering_oct4_mafk_ps.pdf")
 
-plotting.plotTSNEPerSequence_withpie(crbm_merged, test_merged, X, lims,\
+plotting.plotTSNEPerSequence_withpie(crbm_merged, te_merged, X, lims,\
         outputdir + "tsne_clustering_pie_ps.pdf")
 
-plotting.plotTSNEPerSequence_withpie(crbm_merged, test_stem, Xoct4, lims,\
+plotting.plotTSNEPerSequence_withpie(crbm_merged, te_o, Xoct4, lims,\
         outputdir + "oct4_tsne_clustering_pie_ps.pdf")
 
-plotting.plotTSNEPerSequence_withpie(crbm_merged, test_fibro, Xmafk, lims,\
+plotting.plotTSNEPerSequence_withpie(crbm_merged, te_m, Xmafk, lims,\
         outputdir + "mafk_tsne_clustering_pie_ps.pdf")
 
-lab = [ "Oct4"] * test_stem.shape[0] + [ "Mafk" ] * test_fibro.shape[0]
-plotting.violinPlotMotifActivities(crbm_merged, test_merged, labels, filename =\
+lab = [ "Oct4"] * te_o.shape[0] + [ "Mafk" ] * te_m.shape[0]
+plotting.violinPlotMotifActivities(crbm_merged, te_merged, lab, filename =
         outputdir + "violin_oct4_mafk_motif_occurrences.eps")
