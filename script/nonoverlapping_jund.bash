d8e7f44a85ff573f029ebb6556f815a911c9e1ca
wkopp <kopp@molgen.mpg.de>
2017-07-21 11:30:39 +0200

    extracting jund peak regions

--- script/nonoverlapping_jund.bash
+++ script/nonoverlapping_jund.bash
@@ -1,30 +1,29 @@
 junddir=/project/Tf2motif/home/source/crbm/data/jund_data
+cells=(GM12878 H1hesc HeLa-S3 HepG2 K562)
 
 cd $junddir
 
-bedtools intersect -a GM12878_ENCFF002COV.bed -b \
-    H1hesc_ENCFF001UBM.bed HeLa-S3_ENCFF001VIQ.bed \
-    HepG2_ENCFF002CUF.bed K562_ENCFF001VRL.bed -wa -v > \
-    GM12878_only.bed
-bedtools intersect -a H1hesc_ENCFF001UBM.bed -b \
-    GM12878_ENCFF002COV.bed HeLa-S3_ENCFF001VIQ.bed \
-    HepG2_ENCFF002CUF.bed K562_ENCFF001VRL.bed -wa -v > \
-    H1hesc_only.bed
-bedtools intersect -a HeLa-S3_ENCFF001VIQ.bed -b \
-    H1hesc_ENCFF001UBM.bed GM12878_ENCFF002COV.bed \
-    HepG2_ENCFF002CUF.bed K562_ENCFF001VRL.bed -wa -v > \
-    Hela_only.bed
-bedtools intersect -a HepG2_ENCFF002CUF.bed -b \
-    H1hesc_ENCFF001UBM.bed HeLa-S3_ENCFF001VIQ.bed \
-    GM12878_ENCFF002COV.bed K562_ENCFF001VRL.bed -wa -v > \
-    HepG2_only.bed
-bedtools intersect -a K562_ENCFF001VRL.bed -b \
-    H1hesc_ENCFF001UBM.bed HeLa-S3_ENCFF001VIQ.bed \
-    HepG2_ENCFF002CUF.bed GM12878_ENCFF002COV.bed -wa -v > \
-    K562_only.bed
+for  cell in ${cells[*]}; do
+    sort -k 8 -r ${cell}_ENC*.bed |  head -n 4000 > ${cell}_stringent.bed 
+done
 
-cells=(GM12878 H1hesc Hela HepG2 K562)
+for  cell in ${cells[*]}; do
+    rem=(${cells[*]/$cell})
+    remsuf=${rem[@]/%/_stringent.bed}
+    echo "bedtools intersect -v -wa -a ${cell}_stringent.bed -b \
+        ${rem[@]/%/_stringent.bed} > \
+        ${cell}_only.bed"
+    bedtools intersect -v -wa -a ${cell}_stringent.bed -b \
+        ${rem[@]/%/_stringent.bed} > \
+        ${cell}_only.bed
 
-for cell in ${cells[*]}; do
+    bedtools intersect -u -wa -a ${cell}_stringent.bed -b \
+        ${rem[@]/%/_stringent.bed} > \
+        ${cell}_any.bed
+
     python ../../script/extract_fasta_from_bed.py ./ ${cell}_only.bed
 done
+for  cell in ${cells[*]}; do
+    python ../../script/extract_fasta_from_bed.py ./ ${cell}_any.bed
+done
+
